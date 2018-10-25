03bc6da8b99bf452674a9f1d54c4dec6a3490ed7
martinchiri <martinchiricc07@gmail.com>
2018-10-25 13:06:29 -0500

    boton de mapa implementado sin apuntar al sitio correspondiente

--- app/src/main/java/com/martinchiri/saritacoloniaavisos/DetalleActivity.java
+++ app/src/main/java/com/martinchiri/saritacoloniaavisos/DetalleActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 
 public class DetalleActivity extends AppCompatActivity {
     TextView tvDetalleTitulo,tvDetalle,tvDireccion;
-
+    Button btMapa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,15 +17,30 @@ public class DetalleActivity extends AppCompatActivity {
         tvDetalleTitulo=findViewById(R.id.tvDetalleTitulo);
         tvDetalle=findViewById(R.id.tvDetalle);
         tvDireccion=findViewById(R.id.tvDireccion);
+        btMapa=findViewById(R.id.btMapa);
 
         Intent transporte=getIntent();
         final String titulo=transporte.getStringExtra(OfertasActivity.IDTITULO);
         String detalle=transporte.getStringExtra(OfertasActivity.IDDETALLE);
         String direccion=transporte.getStringExtra(OfertasActivity.IDDIRECCION);
+        final double latitud=transporte.getDoubleExtra(OfertasActivity.IDLATITUD,12.0431800);
+        final double longitud=transporte.getDoubleExtra(OfertasActivity.IDLONGITUD,-77.0282400);
 
         tvDetalleTitulo.setText(titulo);
         tvDetalle.setText(detalle);
         tvDireccion.setText(direccion);
+
+        btMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent transporte=new Intent(getApplicationContext(),MapsActivity.class);
+                transporte.putExtra(OfertasActivity.IDLATITUD,latitud);
+                transporte.putExtra(OfertasActivity.IDLONGITUD,longitud);
+                transporte.putExtra(OfertasActivity.IDTITULO,titulo);
+
+                startActivity(transporte);
+            }
+        });
 
     }
 }
